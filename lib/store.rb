0241393583aea13d7d2cb90ac35abac1e73b7276
Wendina <wendina.teng@gmail.com>
2023-05-04 01:28:00 +0000

    tell Active Record that the two tables, employees and stores, are related.

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  # after_create "log_new_store"
+  # after_update "log_update"
+
+  # private
+  # def log_new_store
+  #   puts "A new store was created"
+  # end
+
+  # def log_update
+  #   puts "Store has been updated"
+  # end
 
 end
