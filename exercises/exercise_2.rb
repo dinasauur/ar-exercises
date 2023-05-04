787b3a20d05245ba5336a4d0376abdb28ca0f8e4
Wendina <wendina.teng@gmail.com>
2023-05-04 00:55:53 +0000

    update the first store in database, complete exercise 2.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# find() is usually used to retrieve a row by ID
+# find_by is used as a helper when you're searching for information within a column, and it maps to such with naming conventions.
+# ie. find_by(id: 1)
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.update(name: 'Victoria')
+
+@store1.reload # refresh the object from the database to reflect the change
+puts @store1.name
